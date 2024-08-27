ed08a472b2a80a51e13961bf8f3af861cd56ca17
Nicolás <con23197@uvg.edu.gt>
2024-08-26 18:25:39 -0600

    main funcionando

--- proyecto2/main.c
+++ proyecto2/main.c
@@ -4,32 +4,30 @@
 #include <math.h>
 #include <time.h>
 
-#define PUNTOS_TOTALES 10000
+#define PUNTOS_TOTALES 10000  // Define correctamente la constante
 
 int main(int argc, char const *argv[])
 {
     int puntos_circulo = 0;
-    double x,y,pi_aproximado;
+    double x, y, pi_aproximado;
 
-    //se inicializa para los numeros random
+    // Se inicializa para los números random
     srand(time(NULL));
 
-    for (int i = 0; i <= PUNTOS_TOTALES; i++)
+    for (int i = 0; i < PUNTOS_TOTALES; i++)
     {
-        x = (double)rand()/RAND_MAX;
-        y =  (double)rand()/RAND_MAX;
+        x = (double)rand() / RAND_MAX;
+        y = (double)rand() / RAND_MAX;
 
-        if (x*x + y*y <= 1 )
+        if (x * x + y * y <= 1)
         {
             puntos_circulo++;
         }
-        
     }
 
-    pi_aproximado = 4*(puntos_circulo/PUNTOS_TOTALES);
+    // Asegúrate de que la división se realiza con al menos un número de punto flotante
+    pi_aproximado = 4.0 * puntos_circulo / (double)PUNTOS_TOTALES;
     printf("El valor aproximado de Pi es: %f\n", pi_aproximado);
-
-
 
     return 0;
 }
