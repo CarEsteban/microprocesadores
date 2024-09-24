c21e05e1ffaff1cd518dfb78b32c71f99bf12d93
CarEsteban <car23016@uvg.edu.gt>
2024-09-24 07:16:37 -0600

    inicio lab 7

---
+++ lab7/ejercicio1.cpp
@@ -0,0 +1,12 @@
+/*---------------------------------------
+* UNIVERSIDAD DEL VALLE DE GUATEMALA
+* Sistemas Operativos
+* Autor: Esteban Cárcamo
+* Fecha: 2024/09/24
+*
+* ---------------------------------------
+* Variables de condición p/acceder a 
+* region crítica 
+* variables de condición.
+*----------------------------------------
+*/
